6b4da731d2f73333cedc08964ca6ac80504883bd
mingyu523 <you@example.com>
2019-07-27 16:22:23 +0800

    jquery練習

---
+++ jquery.js
@@ -0,0 +1,19 @@
+// $() 搜尋物件
+// document 網頁文件
+// . 執行
+// function (){} 函式、方法
+
+$(document).ready(function () {
+    
+    alert("練習檔");
+
+    $("h1").hide();
+
+    $(".box").addClass("eee");
+
+    $(".box1").slideUp("2000").slideDown("3000");
+
+    
+
+
+});
